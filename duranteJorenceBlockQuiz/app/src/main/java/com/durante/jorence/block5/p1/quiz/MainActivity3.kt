c51012f7ca35432cd7dbc347305acbf43ba91e70
jorence258 <117345137+jorence258@users.noreply.github.com>
2023-12-08 23:28:36 +0800

    3rd commit
    Instruction button functional

---
+++ duranteJorenceBlockQuiz/app/src/main/java/com/durante/jorence/block5/p1/quiz/MainActivity3.kt
@@ -0,0 +1,11 @@
+package com.durante.jorence.block5.p1.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+    }
+}
